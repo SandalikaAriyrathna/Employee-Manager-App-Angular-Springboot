2907cd23d62c4e7d269bedc6b82d61e6b4c9c4fd
B.M.N.D.S.Ariyarathnna <it20643836@my.sliit.lk>
2022-07-11 20:47:25 +0530

    Employee crud functions

---
+++ employeemanager/src/main/java/tech/getarrays/employeemanager/repo/EmployeeRepo.java
@@ -0,0 +1,13 @@
+package tech.getarrays.employeemanager.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tech.getarrays.employeemanager.model.Employee;
+
+import java.util.Optional;
+
+public interface EmployeeRepo extends JpaRepository<Employee,Long> {
+
+    Optional<Employee> findEmployeeById(Long id);
+
+    void deleteEmployeeById(Long id);
+}
