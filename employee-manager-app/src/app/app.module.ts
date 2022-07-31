8b444a8efe6f2e84ae477ede1dd0c38bcecd6656
B.M.N.D.S.Ariyarathnna <it20643836@my.sliit.lk>
2022-08-01 03:15:50 +0530

    home, login, admin dashboard updated

--- employee-manager-app/src/app/app.module.ts
+++ employee-manager-app/src/app/app.module.ts
@@ -1,22 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { EmployeeService } from './employee.service';
+import { EmployeeService } from './admin-dashboard/containers/admin-dashboard/employee.service';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { AdminDashboardComponent } from './admin-dashboard/containers/admin-dashboard/admin-dashboard.component';
+import { LoginComponent } from './login/containers/login/login.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/containers/home/home.component';
+import { AboutUsComponent } from './about-us/containers/about-us/about-us.component';
 
+const appRoute: Routes = [
+  { path: '', component:  LoginComponent},
+  { path: 'login', component:  LoginComponent},
+  { path: 'home', component:  HomeComponent},
+  { path: 'about-us', component:  AboutUsComponent},
+  { path: 'admin-dashboard', component:  AdminDashboardComponent}
+ 
+ 
+  
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminDashboardComponent,
+    LoginComponent,
+    HomeComponent,
+    AboutUsComponent,
+  
   ],
   imports: [
     CommonModule,
     BrowserModule,  
     HttpClientModule,
     FormsModule,
+    RouterModule.forRoot(appRoute)
   ],
+  exports: [RouterModule],
   providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
